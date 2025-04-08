94634a54944f601e389950b978485203ff2c64db
Dr-AddictStein <dr.addictstein@gmail.com>
2025-04-08 13:08:52 +0600

    all done

--- src/app/auth/layout.js
+++ src/app/auth/layout.js
@@ -1,10 +1,10 @@
 import '@/styles/globals.css';
-import styles from '@/styles/Auth/Layout.module.css';
+// import styles from '@/styles/Auth/Layout.module.css';
 
 export default function AUTHLayout({ children }) {
   return (
-    <div className={styles.pageContainer}>
-      <main className={styles.contentWrapper}>
+    <div >
+      <main>
         {children}
       </main>
     </div>
